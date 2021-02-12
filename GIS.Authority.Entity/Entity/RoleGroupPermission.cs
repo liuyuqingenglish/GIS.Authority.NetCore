759e674bfb6ad257d4d037d0c54dd93612338a0f
liuyuqing <576479931@qq.com>
2021-02-12 19:22:01 +0800

    1.本地文件

---
+++ GIS.Authority.Entity/Entity/RoleGroupPermission.cs
@@ -0,0 +1,42 @@
+﻿using DapperExtensions.Mapper;
+using GIS.Authority.Entity.Base.BaseEntity;
+namespace GIS.Authority.Entity
+{
+    /// <summary>
+    /// 模块
+    /// </summary>
+    public class RoleGroupPermission : OperatorEntity
+    {
+        /// <summary>
+        /// 给XPath设默认值
+        /// </summary>
+        public RoleGroupPermission()
+        {
+        }
+
+        /// <summary>
+        /// 组织id
+        /// </summary>
+        public int RoleGroupId { get; set; }
+
+        /// <summary>
+        /// 模型id
+        /// </summary>
+        public int ModuleId { get; set; }
+    }
+
+    /// <summary>
+    /// Mapper
+    /// </summary>
+    public sealed class RolePermissionMapper : ClassMapper<RoleGroupPermission>
+    {
+        /// <summary>
+        /// DepartmentMapper
+        /// </summary>
+        public RolePermissionMapper()
+        {
+            Table("RolePermission");
+            AutoMap();
+        }
+    }
+}
